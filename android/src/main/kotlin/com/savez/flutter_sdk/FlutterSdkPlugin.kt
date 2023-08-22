7ebcfed7c8c85c35c69b75f31d6a808c62098229
sachin <sachinsabbarwal@gmail.com>
2023-08-22 14:31:21 +0530

    android native side code: WIP

--- android/src/main/kotlin/com/savez/flutter_sdk/FlutterSdkPlugin.kt
+++ android/src/main/kotlin/com/savez/flutter_sdk/FlutterSdkPlugin.kt
@@ -1,6 +1,21 @@
 package com.savez.flutter_sdk
 
+import android.content.Context
 import androidx.annotation.NonNull
+import androidx.security.crypto.MasterKey
+import com.rlynetworkmobilesdk.EncryptedSharedPreferencesHelper
+import org.kethereum.bip39.generateMnemonic
+import org.kethereum.bip39.validate
+import org.kethereum.bip39.dirtyPhraseToMnemonicWords
+import org.kethereum.bip39.toSeed
+import org.kethereum.bip39.wordlists.WORDLIST_ENGLISH
+import org.kethereum.bip39.model.MnemonicWords
+import org.kethereum.bip32.model.ExtendedKey
+import org.kethereum.bip32.toKey
+import org.kethereum.extensions.*
+import java.lang.Integer.parseInt
+
+import com.rlynetworkmobilesdk.MnemonicStorageHelper
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
@@ -9,23 +24,64 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 
 /** FlutterSdkPlugin */
+const val MNEMONIC_STORAGE_KEY = "BIP39_MNEMONIC"
 class FlutterSdkPlugin: FlutterPlugin, MethodCallHandler {
+  private lateinit var prefHelper: EncryptedSharedPreferencesHelper
+  private val MNEMONIC_PREFERENCE_KEY = "BIP39_MNEMONIC"
+
   /// The MethodChannel that will the communication between Flutter and native Android
   ///
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
   /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel : MethodChannel
+  private lateinit var channel: MethodChannel
+  private lateinit var flutterPluginBinding: FlutterPlugin.FlutterPluginBinding
 
   override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
     channel = MethodChannel(flutterPluginBinding.binaryMessenger, "flutter_sdk")
     channel.setMethodCallHandler(this)
+    this.flutterPluginBinding = flutterPluginBinding
+    prefHelper = EncryptedSharedPreferencesHelper(flutterPluginBinding.applicationContext)
   }
 
   override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+
     if (call.method == "getPlatformVersion") {
       result.success("Android ${android.os.Build.VERSION.RELEASE}")
+    }
+    if (call.method == "saveMnemonic") {
+      saveMnemonic(call, result);
+    } else if (call.method == "readMnemonic") {
+      readMnemonic(call, result)
+    } else if (call.method == "deleteMnemonic") {
+      deleteMnemonic(call, result)
+    } else if (call.method == "generateMnemonic") {
+      val phrase = generateMnemonic(192, WORDLIST_ENGLISH)
+      result.success(phrase)
     } else {
       result.notImplemented()
+    }
+  }
+
+  private fun deleteMnemonic(call: MethodCall, result: Result) {
+    val key = call.argument<String?>("key") ?: "null"
+    val mnemonicStorageHelper = MnemonicStorageHelper(flutterPluginBinding.applicationContext)
+    mnemonicStorageHelper.delete(key);
+  }
+
+  private fun readMnemonic(call: MethodCall, result: Result) {
+    val mnemonicStorageHelper = MnemonicStorageHelper(flutterPluginBinding.applicationContext)
+    mnemonicStorageHelper.read(MNEMONIC_STORAGE_KEY, onSuccess = { value ->
+      result.success(value)
+    })
+  }
+
+  private fun saveMnemonic(call: MethodCall, result: Result) {
+    call.argument<String?>("mnemonic")?.let { mnemonic ->
+      if (!MnemonicWords(mnemonic).validate(WORDLIST_ENGLISH)) {
+        result.error("mnemonic_verification_failure", "Mnemonic is not valid", null)
+      }
+      prefHelper.save(MNEMONIC_PREFERENCE_KEY, mnemonic)
+      result.success(true)
     }
   }
 
---
+++ android/src/main/kotlin/com/savez/flutter_sdk/MnemonicStorageHelper.kt
@@ -0,0 +1,107 @@
+package com.rlynetworkmobilesdk
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKey
+import androidx.security.crypto.MasterKey.Builder
+import com.google.android.gms.auth.blockstore.*
+
+class MnemonicStorageHelper(context: Context) {
+    private val sharedPreferences: SharedPreferences
+    private val blockstoreClient: BlockstoreClient
+    private var isEndToEndEncryptionAvailable: Boolean = false;
+
+    init {
+        val masterKey: MasterKey = Builder(context)
+            .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+            .build()
+
+        sharedPreferences = EncryptedSharedPreferences.create(
+            context,
+            "encrypted_mnemonic",
+            masterKey,
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+        )
+
+        blockstoreClient = Blockstore.getClient(context)
+        blockstoreClient.isEndToEndEncryptionAvailable.addOnSuccessListener { isE2EEAvailable ->
+            isEndToEndEncryptionAvailable = isE2EEAvailable
+        }
+    }
+
+    fun save(key: String, mnemonic: String, useBlockstore: Boolean, forceBlockstore: Boolean, onSuccess: () -> Unit, onFailure: (message: String) -> Unit) {
+        if (useBlockstore && isEndToEndEncryptionAvailable) {
+            val storeRequest = StoreBytesData.Builder()
+                .setBytes(mnemonic.toByteArray(Charsets.UTF_8))
+                .setKey(key)
+
+            storeRequest.setShouldBackupToCloud(true)
+
+            blockstoreClient.storeBytes(storeRequest.build())
+                .addOnSuccessListener {
+                    onSuccess()
+                }.addOnFailureListener { e ->
+                    onFailure("Failed to save to cloud $e")
+                }
+        } else {
+            if (forceBlockstore) {
+                onFailure("Failed to save mnemonic. No end to end encryption option is available and force cloud is on");
+            } else {
+                saveToSharedPref(key, mnemonic)
+                onSuccess()
+            }
+        }
+    }
+
+    private fun saveToSharedPref(key: String, mnemonic: String) {
+        val editor = sharedPreferences.edit()
+        editor.putString(key, mnemonic)
+        editor.commit()
+    }
+
+    fun read(key: String, onSuccess: (mnemonic: String?) -> Unit) {
+
+        val retrieveRequest = RetrieveBytesRequest.Builder()
+            .setKeys(listOf(key))
+            .build()
+
+        blockstoreClient.retrieveBytes(retrieveRequest)
+            .addOnSuccessListener { result: RetrieveBytesResponse ->
+                val blockstoreDataMap = result.blockstoreDataMap
+
+                if (blockstoreDataMap.isEmpty()) {
+                    val mnemonic = readFromSharedPref(key)
+                    onSuccess(mnemonic)
+                } else {
+                    val mnemonic = blockstoreDataMap[key]
+                    if (mnemonic !== null) {
+                        onSuccess(mnemonic.bytes.toString(Charsets.UTF_8))
+                    } else {
+                        onSuccess(null)
+                    }
+                }
+            }
+            .addOnFailureListener {
+                val mnemonic = readFromSharedPref(key)
+                onSuccess(mnemonic)
+            }
+    }
+
+    private fun readFromSharedPref(key: String): String? {
+        return sharedPreferences.getString(key, null)
+    }
+
+    fun delete(key: String) {
+        val retrieveRequest = DeleteBytesRequest.Builder()
+            .setKeys(listOf(key))
+            .build()
+
+        blockstoreClient.deleteBytes(retrieveRequest)
+
+        val editor = sharedPreferences.edit()
+        editor.remove(key)
+        editor.commit()
+    }
+}
