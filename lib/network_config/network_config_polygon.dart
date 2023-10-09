6729d4b253b778f6bac76ae523b661629deea532
sachin <sachinsabbarwal@gmail.com>
2023-10-09 12:24:22 +0530

    polygon net config changes

--- lib/network_config/network_config_polygon.dart
+++ lib/network_config/network_config_polygon.dart
@@ -8,10 +8,10 @@ final polygonNetworkConfig = NetworkConfig(
     rlyERC20: '0xe537c642ac8c3136eb8914a047ab3b232f9e695d',
   ),
   gsn: GSNConfig(
-    paymasterAddress: '0x61B9BdF9c10F77bD9eD033559Cec410427aeb8A2',
+    paymasterAddress: '0x29CAa31142D17545C310437825aA4C53FbE621C3',
     forwarderAddress: '0xB2b5841DBeF766d4b521221732F9B618fCf34A87',
     relayHubAddress: '0xfCEE9036EDc85cD5c12A9De6b267c4672Eb4bA1B',
-    relayWorkerAddress: '0x434efbca662f6f846f8dec3fde52fac4c8792e03',
+    relayWorkerAddress: '0x579de7c56cd9a07330504a7c734023a9f703778a',
     relayUrl: 'https://api.rallyprotocol.com',
     rpcUrl:
         'https://polygon-mainnet.g.alchemy.com/v2/-dYNjZXvre3GC9kYtwDzzX4N8tcgomU4',
